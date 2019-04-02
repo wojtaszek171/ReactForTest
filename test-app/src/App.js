f4a97579ac6443c8104f9406087ac4d5dd1d3765
Pawel Wojtaszko <pawelwojtaszko@o2.pl>
2019-04-03 00:08:37 +0200

    created react app

---
+++ test-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import Button from './components/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ test-app/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Button.css'
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="Button">
+        testtest
+      </div>
+    )
+  }
+}
+
+export default Button
